24237bd87f7207a0ad1f26adc9f57fa6b7fa2821
eilinge <17601329166@163.com>
2019-12-02 13:54:19 +0800

    promtheus/build success

--- build-prometheus.sh
+++ build-prometheus.sh
@@ -11,5 +11,6 @@ export PROMETHEUS=/opt/prometheus
 cp ./prometheus.yml ${PROMETHEUS}
 
 mkdir -p /opt/grafana-storage
-
+chmod 777 /opt/grafana-storage -R
+#export GRAFANA=/opt/grafana-storage
 docker-compose -f docker-prometheus.yaml up -d
